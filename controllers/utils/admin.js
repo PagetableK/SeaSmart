7f3d553969799f73e0a9528f11956d4c96175cb6
pagetableK <pablo14gamerx3@gmail.com>
2024-02-15 12:07:02 -0600

    Header Row Intentos

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -8,11 +8,24 @@ const MAIN = document.querySelector('main');
 MAIN.insertAdjacentHTML('beforebegin', `
 <header>
 <nav class="navbar fixed-top" style="background-color: #BCE7FD;">
-<div class="container-fluid">
-  <form class="d-flex" role="search">
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success" type="submit">Search</button>
+<div class="container-fluid mt-5">
+<div class="row">
+<div class="col">
+  <img src="../../resources/img/Logo1.png" width="100px" height="100px col">
+  </div>
+  <div class="col">
+  <form class="d-flex col" role="search">
+    <button class="btn btn-outline-success m-1" type="submit">Calificaciones</button>
+    <button class="btn btn-outline-success m-1" type="submit">Pedidos</button>
+    <button class="btn btn-outline-success m-1" type="submit">Productos</button>
+    <button class="btn btn-outline-success m-1" type="submit">Sub-categorías</button>
+    <button class="btn btn-outline-success m-1" type="submit">Categorías</button>
   </form>
+  </div>
+  <div class="col">
+  <img src="../../resources/img/user.png" width="50px" height="50px" class="m-5 col">
+  </div>
+  </div>
 </div>
 </nav>
 </header>
