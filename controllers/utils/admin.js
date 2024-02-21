670b5cd20ef75443436760bfcf3ac43406a979d9
pagetableK <pablo14gamerx3@gmail.com>
2024-02-21 12:06:23 -0600

    Pruebas

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -7,26 +7,20 @@ const MAIN = document.querySelector('main');
 
 MAIN.insertAdjacentHTML('beforebegin', `
 <header>
-<nav class="navbar fixed-top" style="background-color: #BCE7FD;">
-<div class="container-fluid mt-5">
-<div class="row">
-<div class="col">
-  <img src="../../resources/img/Logo1.png" width="100px" height="100px col">
-  </div>
-  <div class="col">
-  <form class="d-flex col" role="search">
-    <button class="btn btn-outline-success m-1" type="submit">Calificaciones</button>
-    <button class="btn btn-outline-success m-1" type="submit">Pedidos</button>
-    <button class="btn btn-outline-success m-1" type="submit">Productos</button>
-    <button class="btn btn-outline-success m-1" type="submit">Sub-categorías</button>
-    <button class="btn btn-outline-success m-1" type="submit">Categorías</button>
-  </form>
-  </div>
-  <div class="col">
-  <img src="../../resources/img/user.png" width="50px" height="50px" class="m-5 col">
-  </div>
-  </div>
-</div>
-</nav>
+  <nav class="navbar fixed-top" style="background-color: #BCE7FD;">
+    <div class="container justify-content-center">
+      <div class="row">
+        <div class="col">
+          <img src="../../resources/img/Logo1.png" width="100px" height="100px col">
+        </div>
+        <div class="col">
+          
+        </div>
+        <div class="col">
+          <img src="../../resources/img/user.png" width="50px" height="50px" class="m-5 col">
+        </div>
+      </div>
+    </div>
+  </nav>
 </header>
 `);
