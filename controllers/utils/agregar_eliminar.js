b95572bf70a97604a4f246fea73da263777e97fe
PagetableK <pablo14gamerx@gmail.com>
2024-03-07 10:32:54 -0600

    Modal producto, agregar botones eliminar y editar con javascript

--- controllers/utils/agregar_eliminar.js
+++ controllers/utils/agregar_eliminar.js
@@ -1,1 +1,11 @@
-const TD = document.querySelector('main');
+document.getElementById('celda-agregar-eliminar').innerHTML =`<button type="button" class="btn btn-success">
+<img src="../../resources/img/lapiz.png"
+    alt="lapizEditar" width="30px">
+</button>
+<button type="button" class="btn btn-danger">
+<img src="../../resources/img/eliminar.png"
+    alt="lapizEditar" width="30px">
+</button>`;
+
+document.getElementById('fila-producto').setAttribute("data-bs-target", "#modal");
+document.getElementById('fila-producto').setAttribute("data-bs-toggle", "modal");
