aead5106ea9a3d162ec7dd5a5e90e4bd7b99fc1f
adianitafresita <20220125@ricaldone.edu.sv>
2024-03-21 09:44:06 -0600

    login y registro terminado

--- controllers/utils/publico.js
+++ controllers/utils/publico.js
@@ -234,6 +234,75 @@ else if(titulo == "Mi información")
     </header>
     `);
 }
+else if(titulo == "Inicio de sesión")
+{
+    MAIN.insertAdjacentHTML('beforebegin', `
+    <header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+        <div class="container-fluid">
+            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container">
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                    </div>
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <p style="font-weight: 500; font-size: x-large;">S<span
+                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+</header>
+    `);
+}
+else if(titulo == "Registro")
+{
+    MAIN.insertAdjacentHTML('beforebegin', `
+    <header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+        <div class="container-fluid">
+            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container">
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                    </div>
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <p style="font-weight: 500; font-size: x-large;">S<span
+                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+</header>
+    `);
+}
+else if(titulo == "Registro 1")
+{
+    MAIN.insertAdjacentHTML('beforebegin', `
+    <header>
+    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+        <div class="container-fluid">
+            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container">
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                    </div>
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <p style="font-weight: 500; font-size: x-large;">S<span
+                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+</header>
+    `);
+}
 
 let btnCollapse = document.getElementById('btnCollapse');
 let listaCollapse = document.getElementById('listaCollapse');
