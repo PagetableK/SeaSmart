2fa1bbe3db885b9cbbe8e5bbdf9c36df5d48895d
PagetableK <pablo14gamerx3@gmail.com>
2024-04-02 21:46:19 -0600

    Calificaciones, pedidos y modal producto responsivo (Pantalla pequeña)

--- controllers/private/productos.js
+++ controllers/private/productos.js
@@ -1,6 +1,7 @@
 const MODALCPRODUCTO = new bootstrap.Modal('#crearModal_producto');
 const MODALIPRODUCTO = new bootstrap.Modal('#infoModal_producto');
-
+const SEPARADORV = document.getElementById('separadorV');
+const SEPARADORH = document.getElementById('separadorH');
 
 function abrirCrear(){
     MODALCPRODUCTO.show();
@@ -8,4 +9,15 @@ function abrirCrear(){
 
 function abrirInfoProducto(){
     MODALIPRODUCTO.show();
+}
+
+if(window.screen.width <= 430){
+    SEPARADORV.remove();
+    // separacion de los botones de abajo
+}
+else if(window.screen.width < 992){
+    SEPARADORV.remove();
+}
+else{
+    SEPARADORH.remove();
 }
