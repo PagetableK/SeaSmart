4d9ab9b75b359e480d8de1ca6941fa3359788e66
PagetableK <pablo14gamerx3@gmail.com>
2024-04-03 22:59:00 -0600

    Carrito, index y mi_cuenta responsive (público) responsive (Para dispositivos pequeños)

--- controllers/utils/publico.js
+++ controllers/utils/publico.js
@@ -1,11 +1,13 @@
 const MAIN = document.querySelector('main');
 const titulo = document.title;
 
+let validacionElemento_Cerrar;
+
 if (titulo == "SeaSmart") {
     MAIN.insertAdjacentHTML('beforebegin', `
     <header>
         <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
+            <div class="container-fluid row-gap-3">
                 <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
                     <div class="row">
                         <div class="col-12 d-flex align-items-center justify-content-center">
@@ -16,7 +18,7 @@ if (titulo == "SeaSmart") {
                         </div>
                     </div>
                 </div>
-                <div class="col-8 d-flex justify-content-center" id="divBuscador">
+                <div class="col-12 col-sm-8 col-md-8 col-lg-8 d-flex justify-content-center" id="divBuscador">
                     <form class="d-flex w-75" role="busqueda" id="form-buscarCategoria">
                         <input class="form-control" type="buscar" placeholder="Buscar una categoría.."
                             aria-label="Buscar" id="buscarCategoria">
@@ -27,7 +29,7 @@ if (titulo == "SeaSmart") {
                         </div>
                     </form>
                 </div>
-                <div class="col-4 col-xs-2 col-sm-2 col-md-2 d-flex justify-content-end" id="btnCollapse">
+                <div class="col-12 col-sm-2 col-md-2 col-lg-2 d-flex justify-content-end" id="btnCollapse">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#listaCollapse">
                         <span class="navbar-toggler-icon"></span></button>
@@ -42,11 +44,14 @@ if (titulo == "SeaSmart") {
                                 <li id="elemento-registro">
                                     <a href="registro.html" id="btnRegistro_e">Registrarse</a>
                                 </li>
+                                <li id="elemento-carrito">
+                                <a href="carrito.html" id="btnCarrito_e">Mi carrito</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <div class="col-2 d-flex justify-content-center" id="opcionesUsuario">
+                <div class="col-2 p-5 d-flex justify-content-center" id="opcionesUsuario">
                     <div class="dropdown mt-2" id="cuenta">
                         <img src="../../resources/img/user.png" class="dropdown-toggle" type="button" width="25px"
                             height="25px" data-bs-toggle="dropdown" alt="user">
@@ -63,6 +68,7 @@ if (titulo == "SeaSmart") {
         </nav>
     </header>
     `);
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "Mi cuenta")
 {
@@ -103,7 +109,7 @@ else if(titulo == "Mi cuenta")
                                     <a class="pe-5 ps-5 text-center" href="carrito.html" id="btnCarrito_e">Mi carrito</a>
                                 </li>
                                 <li id="elemento-cerrar-sesion">
-                                    <a class="pe-5 ps-5 text-center" href="#" id="btnCerrarSesion_e">Cerrar sesión</a>
+                                    <a class="pe-5 ps-5 text-center" href="index.html" id="btnCerrarSesion_e">Cerrar sesión</a>
                                 </li>
                             </ul>
                         </div>
@@ -128,10 +134,16 @@ else if(titulo == "Mi cuenta")
     </header>
     `);
 
-    if(window.screen.width <= 992)
+    if(window.screen.width >= 992)
     {
-        document.getElementById('menu_cuenta').remove();
+        document.getElementById('menu_cuenta').classList.add('me-4');
     }
+    else
+    {
+        document.getElementById('menu_cuenta').classList.add('mt-4');
+    }
+
+    validacionElemento_Cerrar = true;
 }
 else if(titulo=="Mi carrito")
 {
@@ -166,7 +178,7 @@ else if(titulo=="Mi carrito")
                                     <a href="mi_cuenta.html" id="btnMiCuenta_e">Mi cuenta</a>
                                 </li>
                                 <li id="elemento-cerrar-sesion">
-                                    <a href="#" id="btnCerrarSesion_e">Cerrar sesión</a>
+                                    <a href="index.html" id="btnCerrarSesion_e">Cerrar sesión</a>
                                 </li>
                             </ul>
                         </div>
@@ -193,6 +205,8 @@ else if(titulo=="Mi carrito")
         </nav>
     </header>
     `);
+
+    validacionElemento_Cerrar = true;
 }
 else if(titulo == "Mis pedidos")
 {
@@ -221,11 +235,14 @@ else if(titulo == "Mis pedidos")
                     <div class="container-fluid d-flex justify-content-end">
                         <div class="col-12 d-flex justify-content-center">
                             <ul class="navbar-nav grid gap-3 text-center" id="barra-elementos">
-                                <li id="cerrar-sesion">
-                                    Cerrar sesión
+                                <li id="mi-carrito">
+                                    <a href="mi_cuenta.html" id="btnMiCuenta_e">Mi cuenta</a>
                                 </li>
                                 <li id="mi-carrito">
-                                    Mi carrito
+                                    <a href="carrito.html" id="btnCarrito_e">Mi carrito</a>
+                                </li>
+                                <li id="cerrar-sesion">
+                                    <a href="index.html" id="btnCerrarSesion_e">Cerrar sesión</a>
                                 </li>
                             </ul>
                         </div>
@@ -249,6 +266,8 @@ else if(titulo == "Mis pedidos")
         </nav>
     </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "Mi información")
 {
@@ -305,75 +324,83 @@ else if(titulo == "Mi información")
         </nav>
     </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "Inicio de sesión")
 {
     MAIN.insertAdjacentHTML('beforebegin', `
     <header>
-    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
-        <div class="container-fluid">
-            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
-                <div class="row">
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
-                    </div>
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <p style="font-weight: 500; font-size: x-large;">S<span
-                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+        <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+            <div class="container-fluid">
+                <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
+                    <div class="row">
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                        </div>
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <p style="font-weight: 500; font-size: x-large;">S<span
+                                    style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                        </div>
                     </div>
                 </div>
+                </div>
             </div>
-            </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "Registro")
 {
     MAIN.insertAdjacentHTML('beforebegin', `
     <header>
-    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
-        <div class="container-fluid">
-            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
-                <div class="row">
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
-                    </div>
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <p style="font-weight: 500; font-size: x-large;">S<span
-                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+        <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+            <div class="container-fluid">
+                <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
+                    <div class="row">
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                        </div>
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <p style="font-weight: 500; font-size: x-large;">S<span
+                                    style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                        </div>
                     </div>
                 </div>
+                </div>
             </div>
-            </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "Casi terminamos")
 {
     MAIN.insertAdjacentHTML('beforebegin', `
     <header>
-    <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
-        <div class="container-fluid">
-            <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
-                <div class="row">
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <img src="../../resources/img/Logo1.png" width="55px" height="55px">
-                    </div>
-                    <div class="col-12 d-flex align-items-center justify-content-center">
-                        <p style="font-weight: 500; font-size: x-large;">S<span
-                                style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+        <nav class="navbar navbar-expand-lg" style="background-color: #BCE7FD;">
+            <div class="container-fluid">
+                <div class="col-12 col-sm-2 col-md-2 col-lg-2 col-xl-2" id="seasmart-container" onclick="abrirIndex()">
+                    <div class="row">
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <img src="../../resources/img/Logo1.png" width="55px" height="55px">
+                        </div>
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <p style="font-weight: 500; font-size: x-large;">S<span
+                                    style="color: #3E88DE;">ea</span>S<span style="color: #3E88DE;">mart</span></p>
+                        </div>
                     </div>
                 </div>
+                </div>
             </div>
-            </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 else if(titulo == "¿Quiénes somos?"){
     MAIN.insertAdjacentHTML('beforebegin', `
@@ -396,6 +423,8 @@ else if(titulo == "¿Quiénes somos?"){
         </nav>
     </header>
     `);
+
+    validacionElemento_Cerrar = false;
 }
 
 MAIN.insertAdjacentHTML('afterend', `
@@ -434,11 +463,17 @@ let seasmart = document.getElementById('seasmart-container');
 if (window.screen.width >= 992) {
     btnCollapse.remove();
     listaCollapse.remove();
-    estiloBotonCerrar.setAttribute('style', 'display:none;');
-    estiloBotonCarrito.setAttribute('style', 'display:none;');
+    
 }
 else {
     opcionesUsuario.remove();
+}
+
+if(validacionElemento_Cerrar && window.screen.width >= 992){
+    estiloBotonCerrar.setAttribute('style', 'display:none;');
+    estiloBotonCarrito.setAttribute('style', 'display:none;');
+}
+else if(validacionElemento_Cerrar){
     estiloBotonCerrar.setAttribute('style', 'display:block;');
     estiloBotonCarrito.setAttribute('style', 'display:block;');
 }
