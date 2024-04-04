f857802b997863eb8fe389da116276d0e223c12b
adianitafresita <20220125@ricaldone.edu.sv>
2024-04-03 22:13:22 -0600

    segui con lo de previsualizar agregarle en categorias a los 2

--- resources/js/previsualizar_imagen.js
+++ resources/js/previsualizar_imagen.js
@@ -1,7 +1,23 @@
-const defaultFile = '../../resources/img/hombre_short_rojo.jpg';
+const defaultFile = '../../resources/img/image.png';
 
 const file = document.getElementById( 'foto' );
 const img = document.getElementById('img');
+file.addEventListener( 'change', e => {
+  if(e.target.files [0] ) {
+    const reader = new FileReader( );
+    reader.onload = function( e ) {
+      img.src = e.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }else{
+    img.src = defaultFile;
+  }
+} );
+
+const defaultFile1 = '../../resources/img/image.png';
+
+const file1 = document.getElementById( 'fotou' );
+const img1 = document.getElementById('imge');
 file.addEventListener( 'change', e => {
   if(e.target.files [0] ) {
     const reader = new FileReader( );
