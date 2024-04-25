39d032c2700cc741944306cce82ee48a348c8cf1
pagetableK <pablo14gamerx3@gmail.com>
2024-04-25 09:09:03 -0600

    Cambios de menú lateral y eventos de validaciones en registro

--- controllers/utils/menu_lateral.js
+++ controllers/utils/menu_lateral.js
@@ -2,17 +2,22 @@ const MENU_CUENTA = document.getElementById('menu_cuenta');
 
 MENU_CUENTA.insertAdjacentHTML('afterbegin', `
     <!-- Título de menú -->
-    <div class="d-flex align-items-center justify-content-center gap-2">
+    <div class="row d-flex align-items-center justify-content-center gap-2">
         <!-- Ícono de menú -->
+        <div class="col-auto d-flex">
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" class="bi bi-columns-gap"
-            viewBox="0 0 16 16">
-            <path
-                d="M6 1v3H1V1zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm14 12v3h-5v-3zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM6 8v7H1V8zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm14-6v7h-5V1zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1z" />
-        </svg>
+        viewBox="0 0 16 16">
+        <path
+            d="M6 1v3H1V1zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm14 12v3h-5v-3zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM6 8v7H1V8zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm14-6v7h-5V1zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1z" />
+    </svg>
+        </div>
+        <div  class="col-auto d-flex">
         <p class="p-0 m-0 fs-2">Menú</p>
+        </div>
+        
     </div>
     <!-- Botones del menú -->
-    <div class="d-grid gap-3 mb-5 mt-4">
+    <div class="row gap-3 mb-5 mt-4">
         <!-- Botón mis pedidos -->
         <div class="col d-flex justify-content-center">
             <div class="vr"></div>
